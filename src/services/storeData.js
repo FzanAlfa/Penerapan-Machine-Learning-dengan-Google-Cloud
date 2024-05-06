014b52ee9cae5f8e5a7876ad08b35692d9ef93b6
Fauzan alfa <127025601+FzanAlfa@users.noreply.github.com>
2024-05-06 20:09:26 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -6,4 +6,23 @@ async function storeData(id, data) {
   const predictCollection = db.collection('prediction');
   return predictCollection.doc(id).set(data);
 }
- 
+
+async function getAllData() {
+  const db = new Firestore();
+
+  const predictCollection = db.collection('prediction');
+
+  try {
+    const snapshot = await predictCollection.get();
+    const data = [];
+    snapshot.forEach(doc => {
+      data.push(doc.data());
+    });
+    return data;
+  } catch (error) {
+    console.error('Error saat mengambil data:', error);
+    throw error;
+  }
+}
+
+module.exports = {storeData, getAllData}
